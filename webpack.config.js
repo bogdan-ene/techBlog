92c4f665b2002078c2c7fdf6f576ae3d0909ec0d
Bogdan Ene <bogdan.ionut.ene88@gmail.com>
2023-11-26 19:28:45 +0200

    Add webpack configuration for Node.js core modules

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  resolve: {
+    fallback: {
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      stream: require.resolve('stream-browserify'),
+      util: require.resolve('util/'),
+      buffer: require.resolve('buffer/'),
+      url: require.resolve('url/'),
+      path: require.resolve('path-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      timers: require.resolve('timers-browserify'),
+      os: require.resolve('os-browserify/browser'),
+    },
+  },
 };
